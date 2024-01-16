b5438017fbd88525ca25276ba6d61acebfd4d3c2
ndombrowski <nd.microbiota@gmail.com>
2024-01-16 08:15:02 +0100

    optimized example batch script for crunchomics

--- jobscript.sh
+++ jobscript.sh
@@ -15,29 +15,22 @@ export LC_ALL=en_US.UTF-8
 start=`date "+%s"`
 echo "$SLURM_JOB_NAME started at `date` on node $SLURM_NODEID using $SLURM_CPUS_ON_NODE cpus."
 
+#ensure that conda is recognized
+source ~/.bashrc
 
-## Conda init
-__conda_setup="$('/home/$USER/personal/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/$USER/personal/mambaforge/etc/profile.d/conda.sh" ]; then
-        . "/home/$USER/personal/mambaforge/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/$USER/personal/mambaforge/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-
-## Make sure to use programs on the amplicomics group share
-conda activate /home/ndombro/personal/mambaforge/envs/snakemake_nanoclass
+## Make sure to use the snakemake env installed in the amplicomics share
+conda activate /zfs/omics/projects/amplicomics/miniconda3/envs/snakemake_nanoclass2
 
 srun mkdir -p /scratch/$USER/tmp/
 export TMPDIR=/scratch/$USER/tmp/
 
-## Run nanoclass
-cmd="srun --cores $SLURM_CPUS_ON_NODE snakemake -s /home/ndombro/personal/testing/NanoClass2/Snakefile --configfile config.yaml --use-conda --conda-prefix /home/ndombro/personal/testing/NanoClass2/.snakemake/conda --cores $SLURM_CPUS_ON_NODE --nolock --rerun-incomplete"
+## Run nanoclass2
+cmd="srun --cores $SLURM_CPUS_ON_NODE snakemake \
+    -s /zfs/omics/projects/amplicomics/bin/NanoClass2/Snakefile \
+    --configfile config.yaml --use-conda \
+    --conda-prefix /zfs/omics/projects/amplicomics/bin/NanoClass2/.snakemake/conda \
+    --cores $SLURM_CPUS_ON_NODE --nolock --rerun-incomplete"
+
 echo "Running: $cmd"
 eval $cmd
 
